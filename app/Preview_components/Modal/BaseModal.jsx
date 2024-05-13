dd0128b001c922e8f9e05675bc1d13dcf07706ce
Avegya - Web Development Services <139138920+devyanshyadav@users.noreply.github.com>
2024-05-13 13:44:58 +0530

    fix:modal

--- app/Preview_components/Modal/BaseModal.jsx
+++ app/Preview_components/Modal/BaseModal.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import { motion } from "framer-motion";
@@ -11,6 +11,7 @@ const BaseModal = ({
   openBtn,
   modalTitle,
 }) => {
+  const [mounted, setMounted] = useState(false);
   const mainVariants = {
     open: { visibility: "visible", opacity: 1 },
     close: {
@@ -24,40 +25,46 @@ const BaseModal = ({
     open: { scale: 1 },
     close: { scale: 0.95 },
   };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   return (
     <>
-      <button onClick={() => setOpenModal(true)}>{openBtn}</button>
+      <div className="w-fit" onClick={() => setOpenModal(true)}>
+        {openBtn}
+      </div>
 
-      {createPortal(
-        <motion.main
-          animate={openModal ? "open" : "close"}
-          variants={mainVariants}
-          transition={{ duration: 0.2 }}
-          initial={{ visibility: "hidden", opacity: 0 }}
-          onClick={() => setOpenModal(false)}
-          className="bg-black/50 z-50 fixed filter backdrop-blur-sm inset-0 h-screen w-screen grid place-content-center"
-        >
-          <motion.section
+      {mounted &&
+        createPortal(
+          <motion.main
             animate={openModal ? "open" : "close"}
-            variants={sectionVariants}
+            variants={mainVariants}
             transition={{ duration: 0.2 }}
-            onClick={(e) => e.stopPropagation()}
-            className="relative w-[90vw] flex flex-col md:w-[40vw] min-h-60 rounded-xl shadow-md bg-slate-700 overflow-hidden"
+            onClick={() => setOpenModal(false)}
+            initial={{ visibility: "hidden", opacity: 0 }}
+            className={`bg-black/50 z-50 fixed filter backdrop-blur-md inset-0 h-screen w-screen grid place-content-center`}
           >
-            <div className="w-full  bg-cyan-700/20 font-semibold flex items-center justify-between relative">
-              <h3 className="p-3 text-cyan-400">{modalTitle}</h3>
-              <IoIosCloseCircleOutline
-                onClick={() => setOpenModal(false)}
-                className="absolute top-3 right-3 text-2xl cursor-pointer text-cyan-400 hover:text-cyan-400/50"
-              />
-            </div>
-            <div className="flex items-center justify-center flex-1">
-              {children}
-            </div>
-          </motion.section>
-        </motion.main>,
-        document.body
-      )}
+            <motion.section
+              animate={openModal ? "open" : "close"}
+              variants={sectionVariants}
+              transition={{ duration: 0.2 }}
+              className={`relative w-[90vw] flex flex-col md:w-[40vw] min-h-60 rounded-xl shadow-md shadow-cyan-700/30 bg-slate-700 overflow-hidden`}
+            >
+              <div className="w-full  bg-cyan-700/20 font-semibold flex items-center justify-between relative">
+                <h3 className="p-3 text-cyan-400">{modalTitle}</h3>
+                <IoIosCloseCircleOutline
+                  onClick={() => setOpenModal(false)}
+                  className="absolute top-3 right-3 text-2xl cursor-pointer text-cyan-400 hover:text-cyan-400/50"
+                />
+              </div>
+              <div className="flex items-center justify-center flex-1">
+                {children}
+              </div>
+            </motion.section>
+          </motion.main>,
+          document.body
+        )}
     </>
   );
 };
