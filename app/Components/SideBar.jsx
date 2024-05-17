e153785413b4dacebc60e504440aaa36ed32a0e1
Avegya - Web Development Services <139138920+devyanshyadav@users.noreply.github.com>
2024-05-18 00:35:51 +0530

    fix:modal

--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { GoDotFill } from "react-icons/go";
 import useDevStore from "../libs/zuststore";
 import Link from "next/link";
