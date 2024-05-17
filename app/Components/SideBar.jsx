5cd79a064e56c4e0b7815b3bccf5dcf9676e7bf6
Avegya - Web Development Services <139138920+devyanshyadav@users.noreply.github.com>
2024-05-17 22:43:45 +0530

    made:responsive

--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -7,7 +7,9 @@ import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
 
 const SideBar = () => {
-  const { ComponentData, SideBar } = useDevStore((state) => state);
+  const { ComponentData, SideBar, SideBarToggle } = useDevStore(
+    (state) => state
+  );
   const params = usePathname();
   const data = ComponentData.filter((item) => item.url === params);
 
@@ -22,19 +24,35 @@ const SideBar = () => {
       >
         <ul className="space-y-2 ">
           {ComponentData.map((item, index) => (
-            <li
-              key={index}
-              className={`w-full group flex items-center gap-2 hover:bg-secondary/20 hover:text-white rounded-lg cursor-pointer p-1 px-2 ${
-                data.length > 0 && data[0].name === item.name
-                  ? "text-accent"
-                  : ""
-              } `}
-            >
-              <GoDotFill className="text-xs group-hover:text-accent" />{" "}
-              <Link href={item.url} className="w-full">
-                {item.name}
-              </Link>
-            </li>
+            <>
+              <li
+                key={index}
+                className={`w-full hidden group md:flex items-center gap-2 hover:bg-secondary/20 hover:text-white rounded-lg cursor-pointer p-1 px-2 ${
+                  data.length > 0 && data[0].name === item.name
+                    ? "text-accent"
+                    : ""
+                } `}
+              >
+                <GoDotFill className="text-xs group-hover:text-accent" />{" "}
+                <Link href={item.url} className="w-full">
+                  {item.name}
+                </Link>
+              </li>
+              <li
+                key={index}
+                onClick={() => SideBarToggle()}
+                className={`w-full md:hidden group flex items-center gap-2 hover:bg-secondary/20 hover:text-white rounded-lg cursor-pointer p-1 px-2 ${
+                  data.length > 0 && data[0].name === item.name
+                    ? "text-accent"
+                    : ""
+                } `}
+              >
+                <GoDotFill className="text-xs group-hover:text-accent" />{" "}
+                <Link href={item.url} className="w-full">
+                  {item.name}
+                </Link>
+              </li>
+            </>
           ))}
         </ul>
       </motion.section>
