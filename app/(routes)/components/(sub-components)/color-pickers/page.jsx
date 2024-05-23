df570afa977af869a85a58a8a87f8e9cd985c1c1
Avegya - Web Development Services <139138920+devyanshyadav@users.noreply.github.com>
2024-05-23 17:06:16 +0530

    added one more color picker

--- app/(routes)/components/(sub-components)/color-pickers/page.jsx
+++ app/(routes)/components/(sub-components)/color-pickers/page.jsx
@@ -3,9 +3,12 @@ import ComponentPreview from "@/app/Components/ComponentPreview";
 import ColorPickersInfo from "@/app/Preview_components/ColorPickers/ColorPickersInfo";
 import ReactColor from "@/app/Preview_components/ColorPickers/ReactColor";
 import ReactPickColor from "@/app/Preview_components/ColorPickers/ReactPickColor";
+import ReactColorful from "@/app/Preview_components/ColorPickers/ReactColorful";
 import React, { useState } from "react";
 
 const page = () => {
+  const [hexColor, setHexColor] = useState("#2ed0d7");
+
   const [color, setColor] = useState({
     hex: "#F17013",
   });
@@ -25,6 +28,19 @@ const page = () => {
         }
         component_code={ColorPickersInfo[0].code}
         component_packages={ColorPickersInfo[0].packages}
+      />
+      <ComponentPreview
+        component_name={ColorPickersInfo[2].title}
+        component_details={ColorPickersInfo[2].details}
+        component_usage_code={ColorPickersInfo[2].usage_code}
+        component_preview={
+          <div className="p-4 px-7 rounded-md border border-cyan-700 bg-cyan-700/20">
+            <p className="text-white">{hexColor}</p>
+            <ReactColorful color={hexColor} setColor={setHexColor} />
+          </div>
+        }
+        component_code={ColorPickersInfo[2].code}
+        component_packages={ColorPickersInfo[2].packages}
       />
       <ComponentPreview
         component_name={ColorPickersInfo[1].title}
--- app/Preview_components/ColorPickers/ColorPickersInfo.js
+++ app/Preview_components/ColorPickers/ColorPickersInfo.js
@@ -271,6 +271,154 @@ const ColorPickersInfo = [
         export default ReactPickColor;
         `,
   },
+  {
+    title: "React Colorful",
+    details: {
+      description:
+        "A color picker component that uses react-colorful's HexAlphaColorPicker, offering a modern and interactive way to select colors with alpha channel support.",
+      features: [
+        "Interactive color selection with alpha channel support",
+        "Customizable appearance through styled-components",
+        "Tooltip as popover for color preview",
+        "Hexadecimal color input for direct editing",
+      ],
+      props: [
+        {
+          propName: "color",
+          propExample: "#F17013",
+          propDetail:
+            "The current color state passed down from the parent component. It represents the selected color in hexadecimal format.",
+        },
+        {
+          propName: "setColor",
+          propExample: "(color) => {}",
+          propDetail:
+            "A callback function used to update the color state in the parent component. It receives the new color string as its argument.",
+        },
+      ],
+      packages: [
+        {
+          pckg_name: "react-colorful",
+          pckg_link: "https://www.npmjs.com/package/react-colorful",
+        },
+        {
+          pckg_name: "react-tooltip",
+          pckg_link: "https://www.npmjs.com/package/react-tooltip",
+        },
+        {
+          pckg_name: "styled-components",
+          pckg_link: "https://styled-components.com/",
+        },
+      ],
+    },
+
+    doc_links: ["https://omgovich.github.io/react-colorful/"],
+
+    usage_code: `
+    import ReactColorful from "@/app/Preview_components/ColorPickers/ReactColorful";
+    import React, { useState } from "react";
+    
+    const page = () => {
+      const [hexColor, hexColor] = useState("#2ed0d7");
+    
+    return (
+      <div className="p-4 px-7 rounded-md border border-cyan-700 bg-cyan-700/20">
+        <p className="text-white">{hexColor}</p>
+        <ReactColorful color={hexColor} setColor={hexColor} />
+      </div>
+      );
+    };
+    
+    export default page;
+    `,
+    code: `
+    import React, { useEffect, useState } from "react";
+    import { HexAlphaColorPicker } from "react-colorful";
+    import styled from "styled-components";
+    import { Tooltip as PopOver } from "react-tooltip";
+    import { createPortal } from "react-dom";
+    
+    const PickerDiv = styled.div\`
+      .react-colorful {
+        gap: 5px;
+        width: 100%;
+        height: 180px;
+        border-radius: 0px;
+      }
+      .react-colorful__saturation {
+        border-radius: 5px !important;
+        width: 100%;
+      }
+      .react-colorful__hue,
+      .react-colorful__alpha {
+        border-radius: 5px !important;
+        width: 100%;
+    
+        height: 12px !important;
+      }
+      .react-colorful__hue-pointer,
+      .react-colorful__alpha-pointer,
+      .react-colorful__saturation-pointer {
+        width: 15px !important;
+        height: 15px !important;
+      }
+    \`;
+    const ReactColorful = ({ color, setColor }) => {
+      const [mounted, setMounted] = useState(false);
+      useEffect(() => {
+        setMounted(true);
+      }, []);
+      return (
+        <>
+          {mounted &&
+            createPortal(
+              <PopOver
+                id="colorful-picker-popover"
+                clickable={true}
+                events={["click"]}
+                place="bottom"
+                offset={2}
+                defaultIsOpen={false}
+                delayShow={0}
+                delayHide={0}
+                style={{
+                  backgroundColor: "transparent",
+                  padding: "0px",
+                  zIndex: 1000,
+                }}
+              >
+                <PickerDiv className="your-component bg-slate-700 p-2 border border-cyan-700 rounded-lg w-[200px] space-y-2">
+                  <HexAlphaColorPicker
+                    color={color}
+                    onChange={(e) => setColor(e)}
+                  />
+                  <span className=" flex-col flex flex-1 text-sm text-center text-white">
+                    <input
+                      spellCheck={false}
+                      type="text"
+                      className="w-full px-1 outline-none border-2 border-cyan-700 rounded-md border-transparent focus:border-cyan-400 bg-cyan-700/50"
+                      value={color}
+                      onChange={(e) => setColor(e.target.value)}
+                    />
+                  </span>
+                </PickerDiv>
+              </PopOver>,
+              document.body
+            )}
+          <button
+            data-tooltip-id="colorful-picker-popover"
+            className="min-w-32  min-h-8 rounded-md border-2 border-cyan-700 focus:border-white"
+            style={{
+              backgroundColor: color,
+            }}
+          ></button>
+        </>
+      );
+    };
+    
+    export default ReactColorful;
+    `,
+  },
 ];
 
 export default ColorPickersInfo;
